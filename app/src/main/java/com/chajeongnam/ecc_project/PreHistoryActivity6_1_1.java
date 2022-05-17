b8eb4f795e86fa9a95454db44360f196a1be0f3c
kjlee248 <lkelkj@naver.com>
2022-05-17 22:05:42 +0900

    temp

---
+++ app/src/main/java/com/chajeongnam/ecc_project/PreHistoryActivity6_1_1.java
@@ -0,0 +1,14 @@
+package com.chajeongnam.ecc_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreHistoryActivity6_1_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_history6_1_1);
+    }
+}
