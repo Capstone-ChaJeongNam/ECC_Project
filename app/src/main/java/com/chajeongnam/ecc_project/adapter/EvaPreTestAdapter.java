35f42ee0488a2bf037cb2cb861ad9191b7469ddd
zerohoney <88021388+zerohoney@users.noreply.github.com>
2022-05-19 23:15:35 +0900

    recyclerView: completed adapter class

--- app/src/main/java/com/chajeongnam/ecc_project/adapter/EvaPreTestAdapter.java
+++ app/src/main/java/com/chajeongnam/ecc_project/adapter/EvaPreTestAdapter.java
@@ -1,5 +1,7 @@
 package com.chajeongnam.ecc_project.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chajeongnam.ecc_project.R;
 import com.chajeongnam.ecc_project.model.TempList;
 
 import java.util.ArrayList;
@@ -19,25 +22,43 @@ public class EvaPreTestAdapter extends RecyclerView.Adapter<EvaPreTestAdapter.Vi
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.item_category, parent, false);
+        EvaPreTestAdapter.ViewHolder viewHolder = new EvaPreTestAdapter.ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        TempList tempList = tempLists.get(position);
+        holder.bind(tempList);
     }
+
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tempLists.size();
     }
 
-public  class ViewHolder extends RecyclerView.ViewHolder{
+
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView content;
 
-    public ViewHolder(@NonNull View itemView, TextView content) {
-        super(itemView);
-        this.content = content;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            content = itemView.findViewById(R.id.content);
+        }
+
+        private void bind(TempList tempList) {
+            content.setText(tempList.getTitle());
+        }
     }
-}
+
+
 }
