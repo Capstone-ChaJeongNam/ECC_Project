665a3f7afb263e0910364685fe2656e4cbb555bb
YeonJun Cho <micayel1219@gmail.com>
2022-05-22 21:17:37 +0900

    16th commit : 학생 정보 UI xml 생성, add

---
+++ app/src/main/java/com/chajeongnam/ecc_project/activity/StudentInfoActivity.java
@@ -0,0 +1,41 @@
+package com.chajeongnam.ecc_project.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.chajeongnam.ecc_project.R;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info);
+
+        setActionbar();
+    }
+
+    private void setActionbar(){
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.layout_actionbar);
+        TextView textView = findViewById(R.id.titleTextView);
+        textView.setText("학생 정보");
+        ImageButton imageButton = findViewById(R.id.backImageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
