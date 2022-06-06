b47962da0c2615cb4bb9b704c86d04679b049e6e
zerohoney <88021388+zerohoney@users.noreply.github.com>
2022-06-06 19:59:19 +0900

    14th commit: 포스트 히스토리 20프로 완성

--- app/src/main/java/com/chajeongnam/ecc_project/activity/PostHistoryActivity.java
+++ app/src/main/java/com/chajeongnam/ecc_project/activity/PostHistoryActivity.java
@@ -1,5 +1,6 @@
 package com.chajeongnam.ecc_project.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -19,18 +20,34 @@ import java.util.HashMap;
 import java.util.List;
 
 public class PostHistoryActivity extends AppCompatActivity {
+    private int startYear;
+    private int startMonth;
+    private int startDay;
+    private int endYear;
+    private int endMonth;
+    private int endDay;
+
     private RecyclerView recyclerView;
     private List<TempList> tempLists;
     private PostHistoryAdapter postHistoryAdapter;
-    private ArrayList<HashMap> result=new ArrayList<>();
+    private ArrayList<HashMap> result = new ArrayList<>();
     private Button saveBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_history);
+        Intent intent = getIntent();
+        startYear=intent.getExtras().getInt("startYear");
+        startMonth=intent.getExtras().getInt("startMonth");
+        startDay=intent.getExtras().getInt("startDay");
 
-        tempLists=new ArrayList<>();
+        endYear=intent.getExtras().getInt("endYear");
+        endMonth=intent.getExtras().getInt("endMonth");
+        endDay=intent.getExtras().getInt("endDay");
+        tempLists = new ArrayList<>();
+
+
 
         recyclerView = findViewById(R.id.postHistoryRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(PostHistoryActivity.this));
@@ -38,7 +55,6 @@ public class PostHistoryActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(itemDecoration);
         postHistoryAdapter = new PostHistoryAdapter(tempLists);
         recyclerView.setAdapter(postHistoryAdapter);
-
 
     }
 }
