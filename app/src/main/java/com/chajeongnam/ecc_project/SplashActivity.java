b824cf1336789fdf8018188dab459655538d6793
Park_JuYoung <deu04088@naver.com>
2022-06-06 22:20:50 +0900

    Revert "10th Commit : 자동로그인이 아닌 아이디,비밀번호 기억하기가 되어버린 것..."
    This reverts commit 2b72898cbbf168298747b53667530713877311ae.

--- app/src/main/java/com/chajeongnam/ecc_project/SplashActivity.java
+++ app/src/main/java/com/chajeongnam/ecc_project/SplashActivity.java
@@ -8,12 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class SplashActivity extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,31 +18,18 @@ public class SplashActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        mAuth = FirebaseAuth.getInstance();
 
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
-
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(getApplicationContext(),StartActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            },3000);
-        } else {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-        }
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
--- app/src/main/java/com/chajeongnam/ecc_project/StartActivity.java
+++ app/src/main/java/com/chajeongnam/ecc_project/StartActivity.java
@@ -9,12 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class StartActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +37,5 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
-
-
 }
