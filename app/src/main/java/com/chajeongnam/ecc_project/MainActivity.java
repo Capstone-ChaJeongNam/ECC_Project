635f709bb11effd64edf8b916bac2a32d7eb9d29
Park_JuYoung <deu04088@naver.com>
2022-06-06 22:37:28 +0900

    Revert "Revert "10th Commit : 자동로그인이 아닌 아이디,비밀번호 기억하기가 되어버린 것...""
    This reverts commit b824cf1336789fdf8018188dab459655538d6793.

--- app/src/main/java/com/chajeongnam/ecc_project/MainActivity.java
+++ app/src/main/java/com/chajeongnam/ecc_project/MainActivity.java
@@ -2,13 +2,39 @@ package com.chajeongnam.ecc_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnSignOut;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnSignOut = findViewById(R.id.btn_logout);
+
+        mBtnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "로그아웃하였습니다.", Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+
     }
 }
