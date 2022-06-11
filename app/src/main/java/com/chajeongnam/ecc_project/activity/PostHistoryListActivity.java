828185a0fe77e16af06aa387a1b5b59b2e3049f7
zerohoney <88021388+zerohoney@users.noreply.github.com>
2022-06-11 21:59:24 +0900

    asdasdasd

--- app/src/main/java/com/chajeongnam/ecc_project/activity/PostHistoryListActivity.java
+++ app/src/main/java/com/chajeongnam/ecc_project/activity/PostHistoryListActivity.java
@@ -2,11 +2,8 @@ package com.chajeongnam.ecc_project.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
 import android.widget.Button;
-=======
->>>>>>> 19a76c56b338913a88cb18048c11608329e7c685
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,16 +16,13 @@ import com.chajeongnam.ecc_project.Util.FirebaseData;
 import com.chajeongnam.ecc_project.adapter.PostHistoryListAdapter;
 import com.chajeongnam.ecc_project.decoration.SetItemDecoration;
 import com.chajeongnam.ecc_project.model.PostHistoryDateTempList;
-<<<<<<< HEAD
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-=======
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
->>>>>>> 19a76c56b338913a88cb18048c11608329e7c685
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,23 +43,12 @@ public class PostHistoryListActivity extends AppCompatActivity {
     private TextView start, end;
     private Button goCGraph;
 
-    private DatabaseReference databaseReference;
 
-    private int startYear;
-    private int startMonth;
-    private int startDay;
-
-    private int endYear;
-
-
-    private int endMonth;
-    private int endDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_history_list);
-<<<<<<< HEAD
         tempLists = new ArrayList<>();
         start = findViewById(R.id.getStartDate);
         end = findViewById(R.id.getEndDate);
@@ -134,20 +117,19 @@ public class PostHistoryListActivity extends AppCompatActivity {
             }
         });
 
-=======
         databaseReference = FirebaseDatabase.getInstance().getReference("histories");
 //        firebaseData.setPostHistoryDataFromFirebase();
         TextView startDate = (TextView) findViewById(R.id.getStartDate);
         TextView endDate = (TextView) findViewById(R.id.getEndDate);
-        Intent intent = getIntent();
+        Intent intent2 = getIntent();
         ArrayList<String> date = (ArrayList<String>) getIntent().getSerializableExtra("date");
-        startYear = intent.getExtras().getInt("startYear", 0);
-        startMonth = intent.getExtras().getInt("startMonth", 0);
-        startDay = intent.getExtras().getInt("startDay", 0);
+        startYear = intent2.getExtras().getInt("startYear", 0);
+        startMonth = intent2.getExtras().getInt("startMonth", 0);
+        startDay = intent2.getExtras().getInt("startDay", 0);
 
-        endYear = intent.getExtras().getInt("endYear", 0);
-        endMonth = intent.getExtras().getInt("endMonth", 0);
-        endDay = intent.getExtras().getInt("endDay", 0);
+        endYear = intent2.getExtras().getInt("endYear", 0);
+        endMonth = intent2.getExtras().getInt("endMonth", 0);
+        endDay = intent2.getExtras().getInt("endDay", 0);
 
         startDate.setText(startYear + "/" + startMonth + "/" + startDay + "~");
         endDate.setText("~" + endYear + "/" + endMonth + "/" + endDay);
@@ -161,6 +143,5 @@ public class PostHistoryListActivity extends AppCompatActivity {
         recyclerView.setAdapter(postHistoryListAdapter);
 
 
->>>>>>> 19a76c56b338913a88cb18048c11608329e7c685
     }
 }
